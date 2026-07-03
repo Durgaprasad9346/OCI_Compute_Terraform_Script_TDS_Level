4bacd3de93f8f8fdc9a75252e187440505fb9bb2
Durgaprasad9346 <142432429+Durgaprasad9346@users.noreply.github.com>
2026-07-03 17:43:53 +0530

    Update locals.tf

--- modules/Compute/locals.tf
+++ modules/Compute/locals.tf
@@ -1,12 +1,10 @@
 locals {
 
-  processed_instances = var.instances
-
   block_volume_attachments = flatten([
 
     for inst_name, inst in var.instances : [
 
-      for vol in inst.block_volumes : {
+      for vol in try(inst.block_volumes, []) : {
 
         instance_name   = inst_name
         volume_id       = vol.volume_id
