e7d5649a6b2d95bbf7ef6a5861f9e06c3b2e15ef
Durgaprasad9346 <142432429+Durgaprasad9346@users.noreply.github.com>
2026-07-07 11:19:51 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,11 @@
-output "environment" {
-  value = var.environment
+output "compute_instances" {
+  value = module.compute.instances
+}
+
+output "compute_private_ips" {
+  value = module.compute.private_ips
+}
+
+output "compute_public_ips" {
+  value = module.compute.public_ips
 }
