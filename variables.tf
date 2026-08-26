7616c4cf4e5ec1aa6b57178df64a48e41cf4eb84
Durgaprasad9346 <142432429+Durgaprasad9346@users.noreply.github.com>
2026-08-26 13:17:57 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,29 +1,41 @@
 variable "region" {
-  type = string
+  description = "OCI region"
+  type        = string
 }
 
 variable "environment" {
-  type = string
+  description = "Deployment environment"
+  type        = string
+
+  validation {
+    condition = contains(
+      ["dev", "non-prod", "prod", "shared_it"],
+      lower(var.environment)
+    )
+
+    error_message = "Environment must be one of: dev, non-prod, prod, or shared_it."
+  }
 }
 
 variable "default_compartment_id" {
-  type = string
+  description = "Default OCI compartment OCID"
+  type        = string
 }
 
 variable "default_defined_tags" {
-  type    = map(string)
-  default = {}
+  description = "Default defined tags"
+  type        = map(string)
+  default     = {}
 }
 
 variable "default_freeform_tags" {
-  type    = map(string)
-  default = {}
+  description = "Default freeform tags"
+  type        = map(string)
+  default     = {}
 }
 
 variable "instances" {
-
-  description = "Compute instances configuration"
-
-  type = any
-
+  description = "Compute instance configuration"
+  type        = any
+  default     = {}
 }
